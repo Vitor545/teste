522b99d5756dcdd12e92592adc71737fab1b97f9
Vitor545 <tutornews2@gmail.com>
2023-03-16 09:04:43 -0300

    add

--- src/demo.js
+++ src/demo.js
@@ -14,171 +14,228 @@ const MenuProps = {
   PaperProps: {
     style: {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250
-    }
-  }
+      width: 250,
+    },
+  },
 };
 
-export const consTeste = [{
-  codigo: 'ATT_3874',
-  nomeApresentacao: 'Gás / Fluído refrigerante',
-  dominio: [{
-    codigo: '01',
-    descricao: 'R12',
-  }, {
-    codigo: '02',
-    descricao: 'R1234yf',
-  }, {
-    codigo: '03',
-    descricao: 'R134a',
-  }, {
-    codigo: '04',
-    descricao: 'R22',
-  }, {
-    codigo: '05',
-    descricao: 'R290',
-  }, {
-    codigo: '06',
-    descricao: 'R404A',
-  }, {
-    codigo: '07',
-    descricao: 'R407C',
-  }, {
-    codigo: '08',
-    descricao: 'R410A',
-  }, {
-    codigo: '09',
-    descricao: 'R448A',
-  }, {
-    codigo: '10',
-    descricao: 'R449A',
-  }, {
-    codigo: '11',
-    descricao: 'R452A',
-  }, {
-    codigo: '12',
-    descricao: 'R507',
-  }, {
-    codigo: '13',
-    descricao: 'R513A',
-  }, {
-    codigo: '14',
-    descricao: 'R600a',
-  }, {
-    codigo: '99',
-    descricao: 'Outros',
-  }],
-  objetivos: [{
-    codigo: 7,
-    descricao: 'Produto',
-  }],
-}, {
-  codigo: 'ATT_3878',
-  nomeApresentacao: 'Gás / Fluído refrigerante',
-  dominio: [{
-    codigo: '01',
-    descricao: 'R12',
-  }, {
-    codigo: '02',
-    descricao: 'R1234yf',
-  }, {
-    codigo: '03',
-    descricao: 'R134a',
-  }, {
-    codigo: '04',
-    descricao: 'R22',
-  }, {
-    codigo: '05',
-    descricao: 'R290',
-  }, {
-    codigo: '06',
-    descricao: 'R404A',
-  }, {
-    codigo: '07',
-    descricao: 'R407C',
-  }, {
-    codigo: '08',
-    descricao: 'R410A',
-  }, {
-    codigo: '09',
-    descricao: 'R448A',
-  }, {
-    codigo: '10',
-    descricao: 'R449A',
-  }, {
-    codigo: '11',
-    descricao: 'R452A',
-  }, {
-    codigo: '12',
-    descricao: 'R507',
-  }, {
-    codigo: '13',
-    descricao: 'R513A',
-  }, {
-    codigo: '14',
-    descricao: 'R600a',
-  }, {
-    codigo: '99',
-    descricao: 'Outros',
-  }],
-  objetivos: [{
-    codigo: 7,
-    descricao: 'Produto',
-  }],
-}, {
-  codigo: 'ATT_3879',
-  nomeApresentacao: 'Gás / Fluído refrigerante',
-  dominio: [{
-    codigo: '01',
-    descricao: 'R12',
-  }, {
-    codigo: '02',
-    descricao: 'R1234yf',
-  }, {
-    codigo: '03',
-    descricao: 'R134a',
-  }, {
-    codigo: '04',
-    descricao: 'R22',
-  }, {
-    codigo: '05',
-    descricao: 'R290',
-  }, {
-    codigo: '06',
-    descricao: 'R404A',
-  }, {
-    codigo: '07',
-    descricao: 'R407C',
-  }, {
-    codigo: '08',
-    descricao: 'R410A',
-  }, {
-    codigo: '09',
-    descricao: 'R448A',
-  }, {
-    codigo: '10',
-    descricao: 'R449A',
-  }, {
-    codigo: '11',
-    descricao: 'R452A',
-  }, {
-    codigo: '12',
-    descricao: 'R507',
-  }, {
-    codigo: '13',
-    descricao: 'R513A',
-  }, {
-    codigo: '14',
-    descricao: 'R600a',
-  }, {
-    codigo: '99',
-    descricao: 'Outros',
-  }],
-  objetivos: [{
-    codigo: 7,
-    descricao: 'Produto',
-  }],
-},
+export const consTeste = [
+  {
+    codigo: "ATT_3874",
+    nomeApresentacao: "Gás / Fluído refrigerante",
+    dominio: [
+      {
+        codigo: "01",
+        descricao: "R12",
+      },
+      {
+        codigo: "02",
+        descricao: "R1234yf",
+      },
+      {
+        codigo: "03",
+        descricao: "R134a",
+      },
+      {
+        codigo: "04",
+        descricao: "R22",
+      },
+      {
+        codigo: "05",
+        descricao: "R290",
+      },
+      {
+        codigo: "06",
+        descricao: "R404A",
+      },
+      {
+        codigo: "07",
+        descricao: "R407C",
+      },
+      {
+        codigo: "08",
+        descricao: "R410A",
+      },
+      {
+        codigo: "09",
+        descricao: "R448A",
+      },
+      {
+        codigo: "10",
+        descricao: "R449A",
+      },
+      {
+        codigo: "11",
+        descricao: "R452A",
+      },
+      {
+        codigo: "12",
+        descricao: "R507",
+      },
+      {
+        codigo: "13",
+        descricao: "R513A",
+      },
+      {
+        codigo: "14",
+        descricao: "R600a",
+      },
+      {
+        codigo: "99",
+        descricao: "Outros",
+      },
+    ],
+    objetivos: [
+      {
+        codigo: 7,
+        descricao: "Produto",
+      },
+    ],
+  },
+  {
+    codigo: "ATT_3878",
+    nomeApresentacao: "Gás / Fluído refrigerante",
+    dominio: [
+      {
+        codigo: "01",
+        descricao: "R12",
+      },
+      {
+        codigo: "02",
+        descricao: "R1234yf",
+      },
+      {
+        codigo: "03",
+        descricao: "R134a",
+      },
+      {
+        codigo: "04",
+        descricao: "R22",
+      },
+      {
+        codigo: "05",
+        descricao: "R290",
+      },
+      {
+        codigo: "06",
+        descricao: "R404A",
+      },
+      {
+        codigo: "07",
+        descricao: "R407C",
+      },
+      {
+        codigo: "08",
+        descricao: "R410A",
+      },
+      {
+        codigo: "09",
+        descricao: "R448A",
+      },
+      {
+        codigo: "10",
+        descricao: "R449A",
+      },
+      {
+        codigo: "11",
+        descricao: "R452A",
+      },
+      {
+        codigo: "12",
+        descricao: "R507",
+      },
+      {
+        codigo: "13",
+        descricao: "R513A",
+      },
+      {
+        codigo: "14",
+        descricao: "R600a",
+      },
+      {
+        codigo: "99",
+        descricao: "Outros",
+      },
+    ],
+    objetivos: [
+      {
+        codigo: 7,
+        descricao: "Produto",
+      },
+    ],
+  },
+  {
+    codigo: "ATT_3879",
+    nomeApresentacao: "Gás / Fluído refrigerante",
+    dominio: [
+      {
+        codigo: "01",
+        descricao: "R12",
+      },
+      {
+        codigo: "02",
+        descricao: "R1234yf",
+      },
+      {
+        codigo: "03",
+        descricao: "R134a",
+      },
+      {
+        codigo: "04",
+        descricao: "R22",
+      },
+      {
+        codigo: "05",
+        descricao: "R290",
+      },
+      {
+        codigo: "06",
+        descricao: "R404A",
+      },
+      {
+        codigo: "07",
+        descricao: "R407C",
+      },
+      {
+        codigo: "08",
+        descricao: "R410A",
+      },
+      {
+        codigo: "09",
+        descricao: "R448A",
+      },
+      {
+        codigo: "10",
+        descricao: "R449A",
+      },
+      {
+        codigo: "11",
+        descricao: "R452A",
+      },
+      {
+        codigo: "12",
+        descricao: "R507",
+      },
+      {
+        codigo: "13",
+        descricao: "R513A",
+      },
+      {
+        codigo: "14",
+        descricao: "R600a",
+      },
+      {
+        codigo: "99",
+        descricao: "Outros",
+      },
+    ],
+    objetivos: [
+      {
+        codigo: 7,
+        descricao: "Produto",
+      },
+    ],
+  },
 ];
 
 function getStyles(name, personName, theme) {
@@ -186,7 +243,7 @@ function getStyles(name, personName, theme) {
     fontWeight:
       personName.indexOf(name) === -1
         ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium
+        : theme.typography.fontWeightMedium,
   };
 }
 
@@ -194,47 +251,60 @@ export default function MultipleSelectChip() {
   const theme = useTheme();
   const [stateAll, setStateAll] = React.useState({});
 
-  console.log(stateAll)
+  console.log(stateAll);
 
   const handleChange = (event, selectId) => {
-    const { target: { value } } = event;
-    setStateAll({...stateAll, [selectId]: value})
+    const {
+      target: { value },
+    } = event;
+    setStateAll({ ...stateAll, [selectId]: value });
   };
 
   return (
     <div>
-      {consTeste.map(el => (
-        <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
-        <Select
-          labelId="demo-multiple-chip-label"
-          id="demo-multiple-chip"
-          multiple
-          value={stateAll[el.codigo] || []}
-          onChange={(e) => handleChange(e, el.codigo)}
-          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-          renderValue={(selected) => {
-            const descriptionSelected = el.dominio.filter(el => selected.includes(el.codigo)).map(el => el.descricao)
-            return (
-              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-              {descriptionSelected.map((value) => (
-                <Chip key={value} label={value} />
-              ))}
-            </Box>
-            )
-          }}
-          MenuProps={MenuProps}
+      {consTeste.map((item) => (
+        <FormControl
+          sx={{ width: "20rem", height: "3.3rem", flex: "1 1 auto" }}
         >
-          {el.dominio.map(({descricao, codigo}) => (
-            <MenuItem
-              key={codigo}
-              value={codigo}
-            >
-              {descricao}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+          <InputLabel id="demo-multiple-chip-label">
+            {item.nomeApresentacao}
+          </InputLabel>
+          <Select
+            name={item.codigo}
+            labelId="demo-multiple-chip-label"
+            id="demo-multiple-chip"
+            multiple
+            value={stateAll[item.codigo] || []}
+            onChange={(e) => handleChange(e, item.codigo)}
+            input={
+              <OutlinedInput
+                id="select-multiple-chip"
+                label={item.nomeApresentacao}
+              />
+            }
+            renderValue={(selected) => {
+              const descriptionSelected = item.dominio
+                .filter((el) => selected.includes(el.codigo))
+                .map((el) => el.descricao);
+              return (
+                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                  {descriptionSelected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              );
+            }}
+            MenuProps={MenuProps}
+          >
+            {item.dominio.map(({ codigo, descricao }) => {
+              return (
+                <MenuItem key={codigo} value={codigo}>
+                  {descricao}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
       ))}
     </div>
   );
